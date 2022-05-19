677bb305830b4fb6a3b1c7efc54ac7762c45f479
panel05 <pantlbalazs@gmail.com>
2022-05-19 12:28:45 +0200

    jatekosTipp

--- Torpedo/src/torpedo/Torpedo.java
+++ Torpedo/src/torpedo/Torpedo.java
@@ -7,17 +7,26 @@ public class Torpedo {
     
 
     
+
+    
     private int jatekTipp = 0;
     private String[] hajoHely;
     public static void main(String[] args) {
+        Jatek();
+        
+    }
+    
+    
+    public static void Jatek() {
         jatekosTipp();
     }
     
-    public static void jatekosTipp() {
+    public static int jatekosTipp() {
         int tipp;
         Scanner sc = new Scanner(System.in);
         System.out.println("Kérek egy egész számot: ");
         tipp = sc.nextInt();
-        System.out.println(tipp);
+        return tipp;
     }
+    
 }
